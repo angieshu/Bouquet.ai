67c50e66850a469d0443ac2f88d19e3187ddd1af
Anhelina Shulha <anhelina.shulha@gmail.com>
2017-09-28 10:42:37 -0700

    Moved index file to public

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ var https = require('https');
 var path = require('path');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/buildChart.js',function(req,res){
 	res.sendFile(path.join(__dirname + '/buildChart.js'));
 });
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-	res.redirect('/index.html');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
-
-app.listen(8080);
-console.log("Listening on Port 8080");
